364bbe4a41b61be8fa2b285d2c6a7508c6d27d60
Yuri Subach <ysubach@gmail.com>
2019-10-16 09:40:22 -0600

    minor formatting fixes (for linter)

--- android/src/main/java/com/reactnativecommunity/webview/events/TopHttpErrorEvent.kt
+++ android/src/main/java/com/reactnativecommunity/webview/events/TopHttpErrorEvent.kt
@@ -8,18 +8,18 @@ import com.facebook.react.uimanager.events.RCTEventEmitter
  * Event emitted when a http error is received from the server.
  */
 class TopHttpErrorEvent(viewId: Int, private val mEventData: WritableMap) :
-  Event<TopHttpErrorEvent>(viewId) {
-  companion object {
-    const val EVENT_NAME = "topHttpError"
-  }
+    Event<TopHttpErrorEvent>(viewId) {
+    companion object {
+        const val EVENT_NAME = "topHttpError"
+    }
 
-  override fun getEventName(): String = EVENT_NAME
+    override fun getEventName(): String = EVENT_NAME
 
-  override fun canCoalesce(): Boolean = false
+    override fun canCoalesce(): Boolean = false
 
-  override fun getCoalescingKey(): Short = 0
+    override fun getCoalescingKey(): Short = 0
 
-  override fun dispatch(rctEventEmitter: RCTEventEmitter) =
-    rctEventEmitter.receiveEvent(viewTag, eventName, mEventData)
-
+    override fun dispatch(rctEventEmitter: RCTEventEmitter) {
+        rctEventEmitter.receiveEvent(viewTag, eventName, mEventData)
+    }
 }
